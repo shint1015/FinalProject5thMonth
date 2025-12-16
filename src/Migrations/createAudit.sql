620a737cb4500edcdc5f9558f23fe639cf456fbe
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-12-16 13:53:02 -0800

    fix create
    make it same as in PHPmyAdmin

--- src/Migrations/createAudit.sql
+++ src/Migrations/createAudit.sql
@@ -1,11 +1,11 @@
-CREATE TABLE audit_logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    action VARCHAR(50) NOT NULL,  -- create update deleate
-    entity VARCHAR(50) NOT NULL,       
-    entity_id INT NOT NULL, -- ID        
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    ip VARCHAR(45) DEFAULT NULL,
-    details TEXT,
-    PRIMARY KEY (id)
-);
+CREATE TABLE `audit_logs` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `action` VARCHAR(50) NOT NULL,
+    `entity` VARCHAR(50) NOT NULL,
+    `entity_id` INT NOT NULL,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `ip` VARCHAR(45),
+    `details` TEXT,
+    PRIMARY KEY (`id`)
+)
